1ee81755544bd98fcb0c1af7c6819b82cda61550
Mates <maty.kovarik.cz@gmail.com>
2024-11-10 21:21:24 +0100

    first commit

---
+++ Index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BezvaTábor</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   
+    
+</head>
+<body>
+
+    <!-- Hlavička s navigací -->
+    <header>
+        <!--<div class="logo">LOGO</div> -->
+        <img src="/images/logoBAT.png">
+        <nav>
+            <ul>
+                <li><a href="#">Úvod</a></li>
+                <li><a href="Poradatel.php">Pořadatel</a></li>
+                <li><a href="#">Akce</a></li>
+                <li><a href="#">Dovednosti</a></li>
+                <li><a href="#">Vzkazy</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+   
+
+   
+
+</body>
+</html>
