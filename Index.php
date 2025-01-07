323cf8f81a6e9530406a2957ae00d28d919780bd
Mates <maty.kovarik.cz@gmail.com>
2025-01-08 00:22:16 +0100

    google

--- Index.php
+++ Index.php
@@ -7,6 +7,8 @@ if (session_status() === PHP_SESSION_NONE) {
 // Načtení souborů
 require 'Database.php';
 require 'Functions.php';
+require_once 'vendor/autoload.php';
+
 
 // Připojení k databázi
 $conn = connectToDatabase();
@@ -31,6 +33,36 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['login'])) {
     } else {
         echo $result; // Zobrazení chyby
     }
+}
+
+
+// init configuration
+$clientID = '667754488994-72mh4kcvnfqkh24bs7p4b472mi03d9pf.apps.googleusercontent.com';
+$clientSecret = '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';
+$redirectUri = 'http://localhost/Projekt/Index.php';
+
+// create Client Request to access Google API
+$client = new Google_Client();
+$client->setClientId($clientID);
+$client->setClientSecret($clientSecret);
+$client->setRedirectUri($redirectUri);
+$client->addScope("email");
+$client->addScope("profile");
+
+// authenticate code from Google OAuth Flow
+if (isset($_GET['code'])) {
+  $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+  $client->setAccessToken($token['access_token']);
+
+  // get profile info
+  $google_oauth = new Google_Service_Oauth2($client);
+  $google_account_info = $google_oauth->userinfo->get();
+  $email =  $google_account_info->email;
+  $name =  $google_account_info->name;
+
+  // now you can use this profile info to create account in your website and make user logged in.
+} else {
+  echo "<a href='".$client->createAuthUrl()."'>Google Login</a>";
 }
 
 $conn->close();
