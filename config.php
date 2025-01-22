1e7c403b71a3790d9694d8cc2920aa32585b33fa
MaTes20 <maty.kovarik.cz@gmail.com>
2025-01-23 00:18:39 +0100

    google login update

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ require_once 'vendor/autoload.php';
 // init configuration
 $clientID = '667754488994-72mh4kcvnfqkh24bs7p4b472mi03d9pf.apps.googleusercontent.com';
 $clientSecret = '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';
-$redirectUri = 'http://localhost/Projekt/Index.php';
+$redirectUri = 'http://localhost:8080/Projekt/Index.php';
 
 // create Client Request to access Google API
 $client = new Google_Client();
